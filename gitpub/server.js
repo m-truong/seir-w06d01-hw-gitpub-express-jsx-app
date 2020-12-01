d7bc5f578b4292dab7a8098fd2537fcc88233527
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-11-30 19:12:42 -0500

    added drinks.js to models directory with drinks json

--- gitpub/server.js
+++ gitpub/server.js
@@ -3,11 +3,19 @@ const app = express()
 // cap_case is true constant
 const PORT = 3000;
 
+// requires the drinks-modal exports data! 
+    // stores the data
+const drinks = require('./models/drinks.js')
+
 // INDEX-Route that shows ALL resources
 app.get('/', (req, res) => {
     res.send(`<h1 style="font-size: 30px; color: red;"> Welcome to the GitPub App</h1>`)
 })
 
+// 
+app.get('/drinks', (req, res) => {
+    res.send(drinks); 
+})
 // SHOW-route that shows a single resource
 
 
