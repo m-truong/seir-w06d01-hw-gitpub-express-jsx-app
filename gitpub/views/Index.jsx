2fa1b943af942799bd2bd12a9654c8780b76f03f
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-11-30 19:19:02 -0500

    required React npm package inside Index.jsx; and set up React class-component

---
+++ gitpub/views/Index.jsx
@@ -0,0 +1,15 @@
+// require React npm package
+const React = require('react')
+
+// create React Class-Component
+class Index extends React.Component {
+
+    render () {
+        // return {
+
+        // }
+    }
+}
+
+// export Index React class-component
+module.exports = Index;
