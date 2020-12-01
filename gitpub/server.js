ac0155824176d13c7dbaf5271894e5dd5f9db5ff
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-11-30 19:15:52 -0500

    setup server.js to use jsx view-engine

--- gitpub/server.js
+++ gitpub/server.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 // cap_case is true constant
 const PORT = 3000;
+// creates JSX view-engine
+const viewEngine = require('express-react-views').createEngine
 
 // requires the drinks-modal exports data! 
-    // stores the data
+// stores the data
 const drinks = require('./models/drinks.js')
+
+// make server.js to work with JSX template files using JSX view-engine
+app.set('view engine', 'jsx')
+app.engine('jsx', viewEngine())
 
 // INDEX-Route that shows ALL resources
 app.get('/', (req, res) => {
